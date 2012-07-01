69ffa6d87b5adf96e6b2e83fb604c636480cfc56
Carl Chatfield <carlchatfield@gmail.com>
2012-07-01 11:45:06 +1000

    Casts now act on pointers.

--- glmat33/cast.go
+++ glmat33/cast.go
@@ -9,24 +9,24 @@ func (m *Mat4) F(mf *Mat4f) {
 }
 
 // Create a Vec4f from a a Vec4.
-func (v Vec4) F(vf Vec4f) {
-	vf[0] = float32(v[0])
-	vf[1] = float32(v[1])
-	vf[2] = float32(v[2])
-	vf[3] = float32(v[3])
+func (v *Vec4) F(vf Vec4f) {
+	vf[0] = float32((*v)[0])
+	vf[1] = float32((*v)[1])
+	vf[2] = float32((*v)[2])
+	vf[3] = float32((*v)[3])
 }
 
 // Create a Vec3f from a a Vec3.
-func (v Vec3) F(vf Vec3f) {
-	vf[0] = float32(v[0])
-	vf[1] = float32(v[1])
-	vf[2] = float32(v[2])
+func (v *Vec3) F(vf Vec3f) {
+	vf[0] = float32((*v)[0])
+	vf[1] = float32((*v)[1])
+	vf[2] = float32((*v)[2])
 }
 
 // Create a Vec2f from a a Vec2.
-func (v Vec2) F(vf Vec2f) {
-	vf[0] = float32(v[0])
-	vf[1] = float32(v[1])
+func (v *Vec2) F(vf Vec2f) {
+	vf[0] = float32((*v)[0])
+	vf[1] = float32((*v)[1])
 }
 
 // Create a Mat4 from a a Mat4f.
@@ -38,22 +38,22 @@ func (mf *Mat4f) D(m *Mat4) {
 }
 
 // Create a Vec4 from a a Vec4f.
-func (vf Vec4f) D(v Vec4) {
-	v[0] = float64(vf[0])
-	v[1] = float64(vf[1])
-	v[2] = float64(vf[2])
-	v[3] = float64(vf[3])
+func (vf *Vec4f) D(v Vec4) {
+	v[0] = float64((*vf)[0])
+	v[1] = float64((*vf)[1])
+	v[2] = float64((*vf)[2])
+	v[3] = float64((*vf)[3])
 }
 
 // Create a Vec3 from a a Vec3f.
-func (vf Vec3f) D(v Vec3) {
-	v[0] = float64(vf[0])
-	v[1] = float64(vf[1])
-	v[2] = float64(vf[2])
+func (vf *Vec3f) D(v Vec3) {
+	v[0] = float64((*vf)[0])
+	v[1] = float64((*vf)[1])
+	v[2] = float64((*vf)[2])
 }
 
 // Create a Vec2 from a a Vec2f.
-func (vf Vec2f) D(v Vec2) {
-	v[0] = float64(vf[0])
-	v[1] = float64(vf[1])
+func (vf *Vec2f) D(v Vec2) {
+	v[0] = float64((*vf)[0])
+	v[1] = float64((*vf)[1])
 }
