795f25c81906a13c2cec40e9956c3b88f4fcb2c3
Carl Chatfield <carlchatfield@gmail.com>
2013-10-18 11:13:33 +1100

    Added AngleAxis.

---
+++ glmat42/angleaxis.go
@@ -0,0 +1,40 @@
+package glmat42
+
+import (
+	"math"
+)
+
+func AngleAxis4(angle float64, axis Vec3) (m *Mat4) {
+	m = new(Mat4)
+	m.AngleAxis(angle, axis)
+	return m
+}
+
+func (m *Mat4) AngleAxis(angle float64, u Vec3) *Mat4 {
+	c := math.Cos(angle)
+	s := math.Sin(angle)
+	c1 := 1-c
+
+	m[ 0] = c + u[0] * u[0] * c1
+	m[ 1] = u[1] * u[0] * c1 + u[2] * s
+	m[ 2] = u[2] * u[0] * c1 - u[1] * s
+	m[ 3] = 0
+
+	m[ 4] = u[0] * u[1] * c1 - u[2] * s
+	m[ 5] = c + u[1] * u[1] * c1
+	m[ 6] = u[2] * u[1] * c1 + u[0] * s
+	m[ 7] = 0
+
+	m[ 8] = u[0] * u[2] * c1 + u[1] * s
+	m[ 9] = u[1] * u[2] * c1 - u[0] * s
+	m[10] = c + u[2] * u[2] * c1
+	m[11] = 0
+
+	m[13] = 0
+	m[14] = 0
+	m[12] = 0
+	m[15] = 1
+
+	return m
+}
+
